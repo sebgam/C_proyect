4360f65506d07039a29243445485323ddc9cb98e
sebas <sebastiangamboa11@gmail.com>
2017-02-20 19:14:41 -0500

    multidi

---
+++ Array_Multidimencional/Array_Multidimencional/clsMatrices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Multidimencional
+{
+    public class clsMatrices
+    {
+        private int[,] matriz;
+
+        public clsMatrices()
+        {
+            matriz = new int[4, 2];
+
+
+        }
+
+
+    }
+
+}
